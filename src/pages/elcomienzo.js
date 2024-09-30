74273d749f087852bc5a5a42de92642b9dca1d0d
Sergio <ortiz.sergio0303@gmail.com>
2024-09-30 14:26:37 -0400

    feat(routes): el comienzo presave redirect

---
+++ src/pages/elcomienzo.js
@@ -0,0 +1,5 @@
+export const prerender = false;
+
+export function GET({ redirect }) {
+  return redirect('https://distrokid.com/hyperfollow/mateoferro/el-comienzo?utm_campaign=website&utm_medium=Email+&utm_source=SendGrid', 302);
+}
